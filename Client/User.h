2d5e4545feb4d395ab4e18748b94c488b864dfca
Leo-TheCoder <64176716+Leo-TheCoder@users.noreply.github.com>
2020-06-15 20:10:22 +0700

    MultiClient

---
+++ Client/User.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class User
+{
+private:
+	string username;
+	string password;
+	bool status;		//Trạng thái hoạt động (1: online, 0: offline)
+public:
+	void setUsername(string username);
+	void setPassword(string password);
+	void setStatus(bool stt);
+	string getUsername();
+	string getPassword();
+	bool getStatus();
+	bool operator ==(User a);
+
+	User();
+};
+
+class ListUser
+{
+private:
+	vector<User> listUsers;
+public:
+	bool existUsername(string username);
+
+	//Đọc file để lấy danh sách tk
+	void updateList();
+	//Xuất ra thông tin tk lên console(để test thôi)
+	void writeAll();
+	//Kiểm tra trạng thái để đăng nhập
+	//Ret 1: có thể đăng nhập
+	//Ret 0: không có user này(nhập sai)
+	//Ret 2: user đang hoạt động
+	int checkList(User guess);
+	//Dùng username để get user
+	User* getUserByName(string);
+};
---
+++ Server/User.cpp
@@ -0,0 +1,122 @@
+﻿#include "User.h"
+
+void User::setUsername(string name)
+{
+	username = name;
+}
+void User::setPassword(string pass)
+{
+	password = pass;
+}
+void User::setStatus(bool stt)
+{
+	status = stt;
+}
+string User::getPassword()
+{
+	return password;
+}
+string User::getUsername()
+{
+	return username;
+}
+bool User::getStatus()
+{
+	return status;
+}
+
+User::User()
+{
+	username = "";
+	password = "";
+	status = false;
+}
+
+
+void ListUser::updateList()
+{
+	string temp;
+	string username;
+	string password;
+	vector<User> newList;
+	freopen("accounts.txt", "r", stdin);
+	//Đơn giản là đọc file
+	//Các tk sẽ có dạng "username,password"
+	//Mỗi dòng là mỗi tk
+	while (!cin.eof())
+	{
+		User s;
+		getline(cin, temp);
+		int i_separate = temp.find(',');
+		username = temp.substr(0, i_separate);
+		password = temp.substr(i_separate + 1);
+		s.setUsername(username);
+		s.setPassword(password);
+		newList.push_back(s);
+	}
+
+	listUsers = newList;
+
+	cin.clear();
+	cin.ignore();
+	freopen("CON", "rt", stdin);
+	cin.clear();
+}
+
+bool User::operator==(User a)
+{
+	if (this->username == a.username &&
+		this->password == a.password)
+	{
+		return true;
+	}
+	return false;
+}
+
+void ListUser::writeAll()
+{
+	for (int i = 0; i < listUsers.size(); i++)
+	{
+		cout << "Username: " << listUsers[i].getUsername() << endl;
+		cout << "Password: " << listUsers[i].getPassword() << endl;
+		cout << endl;
+	}
+}
+
+//Kiểm tra trạng thái đăng nhập
+int ListUser::checkList(User guess)
+{
+	for (int i = 0; i < listUsers.size(); i++)
+	{
+		//Tìm ra rồi nè
+		if (guess == listUsers[i])
+		{
+			//Xem có được xài hay chưa
+			if (listUsers[i].getStatus())
+			{
+				//Đm thằng nào lấy acc t @@
+				return 2;
+			}
+			else
+			{	//Ok acc này còn xài được
+				listUsers[i].setStatus(true);	//T sẽ xí cái acc này
+				return 1;
+			}
+		}
+	}
+	//Nếu tìm k ra thì thôi !!
+	return 0;
+}
+
+User* ListUser::getUserByName(string name)
+{
+	User* res = NULL;
+	for (int i = 0; listUsers.size(); i++)
+	{
+		if (name == listUsers[i].getUsername())
+		{
+			res = &listUsers[i];
+		}
+	}
+	return res;
+}
