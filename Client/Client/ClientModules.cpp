31cb2881002a5a2bba57c730a992fc5303e851f7
duclam227 <55783513+duclam227@users.noreply.github.com>
2020-06-15 20:09:59 +0700

    Update ClientModules.cpp

--- Client/Client/ClientModules.cpp
+++ Client/Client/ClientModules.cpp
@@ -1,4 +1,4 @@
-﻿#include "ClientModules.h"
+#include "ClientModules.h"
 
 
 /*
@@ -36,14 +36,62 @@ int LogIn(SOCKET sock)
 				// Wait for response
 				ZeroMemory(receivedContent, 4096);
 				bytesReceived = recv(sock, receivedContent, 4096, 0);
+				string recv(receivedContent);
 				if (bytesReceived > 0)
 				{
-					if (receivedContent == "1") {
+					if (recv == "1") {
 						cout << "Wrong username or password!\n";
 						return 1;
 					}
 					else {
 						cout << "Logged in successfully!\n";
+						return 0;
+					}
+				}
+				else {
+					return 1;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+int Register(SOCKET sock)
+{
+	string username, password;
+	int sendResult; //biến giữ giá trị trả về của hàm send
+	char* receivedContent = new char[4096];
+	int bytesReceived;
+
+	cout << "Enter username and password to register.\n";
+	cout << "Username: ";
+	cin >> username;
+	cout << "Password: ";
+	cin >> password;
+
+	if ((username.length() > 0) && (password.length() > 0))		// Make sure the user has typed in something
+	{
+		// Send the text
+		sendResult = send(sock, username.c_str(), username.size() + 1, 0);
+		cout << " ";
+		if (sendResult != SOCKET_ERROR)
+		{
+			sendResult = send(sock, password.c_str(), password.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR) {
+				// Wait for response
+				ZeroMemory(receivedContent, 4096);
+				bytesReceived = recv(sock, receivedContent, 4096, 0);
+				string recv(receivedContent);
+				if (bytesReceived > 0)
+				{
+					if (recv == "1") {
+						cout << "Username not available!\n";
+						return 1;
+					}
+					else {
+						cout << "Registered successfully!\n";
 						return 0;
 					}
 				}
